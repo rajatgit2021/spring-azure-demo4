70169ac8eae19abd703485856d055dcf8ca5c5ec
rajatgit2021 <panda.rajat2018.com>
2022-12-09 00:59:09 +0530

    First commit

---
+++ src/main/java/com/rajat04/springazuredemo4/SpringAzureDemo4Application.java
@@ -0,0 +1,13 @@
+package com.rajat04.springazuredemo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureDemo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureDemo4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rajat04/springazuredemo4/SpringAzureDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat04.springazuredemo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
